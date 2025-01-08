b8e8557fe938159b7bac06eb7ac500776a7a1660
max <maximilian.mantel@tu-berlin.de>
2025-01-08 18:21:58 +0100

    feat: create rail database

---
+++ database/db_rail.py
@@ -0,0 +1,6 @@
+"""Defines all rail components."""
+RAIL_DATABASE = {
+    "UIC60": {"Br": 5000.0, "dr": 0.5, "mr": 60.0, "rl_geo": [(0.0, 0.0)]},
+    "UIC54": {"Br": 4500.0, "dr": 0.45, "mr": 54.0, "rl_geo": [(0.0, 0.0)]},
+    # Add more rail profiles and their attributes here
+}
