18a0535bae2cbebff642251bb72a994c34d88eaf
max <maximilian.mantel@tu-berlin.de>
2025-01-08 18:22:18 +0100

    test: new Rail class test

--- tests/components/test_rail.py
+++ tests/components/test_rail.py
@@ -10,9 +10,9 @@ def test_rail_initialization():
     assert rail.rl_prof == "UIC60"
     assert rail.rl_geo == [(0.0, 0.0)]
     assert rail.rl_num == 1
-    assert rail.Br == 0.0
-    assert rail.dr == 0.0
-    assert rail.mr == 0.0
+    assert rail.Br == 5000.0
+    assert rail.dr == 0.5
+    assert rail.mr == 60.0
 
 def test_rail_invalid_number():
     """Test the initialization of the Rail class with invalid rail number."""
@@ -21,10 +21,10 @@ def test_rail_invalid_number():
 
 def test_rail_set_values():
     """Test the setting of values in the Rail class."""
-    rail = Rail(rl_prof="UIC54", rl_geo=[(1.0, 1.0)], rl_num=2, Br=1000.0, dr=10.0, mr=50.0)
-    assert rail.rl_prof == "UIC54"
-    assert rail.rl_geo == [(1.0, 1.0)]
-    assert rail.rl_num == 2
-    assert rail.Br == 1000.0
-    assert rail.dr == 10.0
-    assert rail.mr == 50.0
+rail = Rail(rl_prof="UIC54", rl_geo=[(0.0, 0.0)], rl_num=2, Br=4500.0, dr=0.45, mr=54.0)
+assert rail.rl_prof == "UIC54"
+assert rail.rl_geo == [(0.0, 0.0)]
+assert rail.rl_num == 2
+assert rail.Br == 4500.0
+assert rail.dr == 0.45
+assert rail.mr == 54.0
