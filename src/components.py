8c44bcbe57e9b5ad153da2633432e0866cbfb508
max <maximilian.mantel@tu-berlin.de>
2025-01-09 17:59:11 +0100

    upd: add properties to Rail class

--- src/components.py
+++ src/components.py
@@ -1,6 +1,6 @@
 """Defines all superstructure components."""
 
-from traitlets import Enum, Float, HasTraits, Int, List, Tuple, Unicode
+from traitlets import Enum, Float, HasTraits, Int, List, Tuple
 
 from database.db_rail import RAIL_DATABASE
 
@@ -17,14 +17,47 @@ class Rail(HasTraits):
     # Rail outline coordinates [m]
     rl_geo = List(Tuple(Float(), Float()), default_value=[(0.0, 0.0)], minlen=1)
 
-    # Rail bending stiffness [Nm^2]
-    Br = Float()
+    # Beam type (TS: Timoshenko, EB: Euler-Bernoulli)
+    bm_type = Enum(["TS", "EB"], default_value="TS").tag(config=True)
+
+    # Elastic modulus of rail [Pa]
+    E = Float()
+
+    # Shear modulus of rail [Pa]
+    G = Float()
+
+    # Poisson's ratio of rail [-]
+    nu = Float()
+
+    # Timoshenko shear correction factor [-]
+    kap = Float()
+
+    # Rail mass per unit length [kg/m]
+    mr = Float()
 
     # Rail damping coefficient [Ns/m]
     dr = Float()
 
-    # Rail mass per unit length [kg/m]
-    mr = Float()
+    # Rail shear center with respect to centroid in z-direction [m]
+    gammar = Float()
+
+    # Area moment of inertia of rail around y-axis [m^4]
+    Iyr = Float()
+
+    # Area moment of inertia of rail around z-axis [m^4]
+    Izr = Float()
+
+    # Torsional constant of rail [m^4]
+    Itr = Float()
+
+    # Cross-sectional area of rail [m^2]
+    Ar = Float()
+
+    # Surface area per unit length of rail [m^2/m]
+    Asr = Float()
+
+    # Volume per unit length of rail [m^3/m]
+    Vr = Float()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -33,9 +66,19 @@ class Rail(HasTraits):
     def set_attributes_from_profile(self):
         attributes = RAIL_DATABASE.get(self.rl_prof, {})
         self.rl_geo = attributes.get("rl_geo", [(0.0, 0.0)])
-        self.Br = attributes.get("Br", 0.0)
-        self.dr = attributes.get("dr", 0.0)
+        self.E = attributes.get("E", 0.0)
+        self.G = attributes.get("G", 0.0)
+        self.nu = attributes.get("nu", 0.0)
+        self.kap = attributes.get("kap", 0.0)
         self.mr = attributes.get("mr", 0.0)
+        self.dr = attributes.get("dr", 0.0)
+        self.gammar = attributes.get("gammar", 0.0)
+        self.Iyr = attributes.get("Iyr", 0.0)
+        self.Izr = attributes.get("Izr", 0.0)
+        self.Itr = attributes.get("Itr", 0.0)
+        self.Ar = attributes.get("Ar", 0.0)
+        self.Asr = attributes.get("Asr", 0.0)
+        self.Vr = attributes.get("Vr", 0.0)
 
 
 class Pad(HasTraits):
