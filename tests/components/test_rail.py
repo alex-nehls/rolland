edcd66a0f7512c8046d75e75a55af3648dac6e44
max <maximilian.mantel@tu-berlin.de>
2025-01-09 18:03:44 +0100

    Reapply "upd: add properties to Rail class"
    This reverts commit f97f67cee9e1207297e62e4af3e469d42e5604e8.

--- tests/components/test_rail.py
+++ tests/components/test_rail.py
@@ -1,30 +1,40 @@
 import pytest
-from traitlets import TraitError
-
 from src.components import Rail
-
 
 def test_rail_initialization():
     """Test the initialization of the Rail class."""
     rail = Rail()
     assert rail.rl_prof == "UIC60"
-    assert rail.rl_geo == [(0.0, 0.0)]
     assert rail.rl_num == 1
-    assert rail.Br == 5000.0
-    assert rail.dr == 0.5
-    assert rail.mr == 60.0
+    assert rail.bm_type == "TS"
+    assert rail.E == 210e9
+    assert rail.G == 81e9
+    assert rail.nu == 0.3
+    assert rail.kap == 0.0
+    assert rail.mr == 60.2
+    assert rail.dr == 0.0
+    assert rail.gammar == 0.0
+    assert rail.Iyr == 3038.30e-8
+    assert rail.Izr == 512.30e-8
+    assert rail.Itr == 209.20e-8
+    assert rail.Ar == 76.70e-4
+    assert rail.Asr == 0.688
+    assert rail.Vr == 7670.00e-6
 
-def test_rail_invalid_number():
-    """Test the initialization of the Rail class with invalid rail number."""
-    with pytest.raises(TraitError):
-        Rail(rl_num=3)
-
-def test_rail_set_values():
-    """Test the setting of values in the Rail class."""
-rail = Rail(rl_prof="UIC54", rl_geo=[(0.0, 0.0)], rl_num=2, Br=4500.0, dr=0.45, mr=54.0)
-assert rail.rl_prof == "UIC54"
-assert rail.rl_geo == [(0.0, 0.0)]
-assert rail.rl_num == 2
-assert rail.Br == 4500.0
-assert rail.dr == 0.45
-assert rail.mr == 54.0
+def test_rail_set_profile():
+    """Test setting the rail profile."""
+    rail = Rail(rl_prof="UIC54")
+    assert rail.rl_prof == "UIC54"
+    assert rail.E == 0.0
+    assert rail.G == 0.0
+    assert rail.nu == 0.0
+    assert rail.kap == 0.0
+    assert rail.mr == 54.0
+    assert rail.dr == 0.0
+    assert rail.gammar == 0.0
+    assert rail.Iyr == 0.0
+    assert rail.Izr == 0.0
+    assert rail.Itr == 0.0
+    assert rail.Ar == 0.0
+    assert rail.Asr == 0.0
+    assert rail.Vr == 0.0
