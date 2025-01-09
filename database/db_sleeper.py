31524b7de27f7d7bb00daeb9fed18fa12b0104f7
max <maximilian.mantel@tu-berlin.de>
2025-01-09 09:51:00 +0100

    Revert "feat: create sleeper database"
    This reverts commit 3125b39b69c1caaec429b242b30b548873315886.

--- database/db_sleeper.py
+++
@@ -1,6 +0,0 @@
-"""Defines all superstructure components."""
-SLEEPER_DATABASE = {
-    "B70": {"ms": 300.0, "ls": 2.5, "Bs": 10000.0},
-    "B80": {"ms": 320.0, "ls": 2.6, "Bs": 11000.0},
-    # Add more sleeper types and their attributes here
-}
