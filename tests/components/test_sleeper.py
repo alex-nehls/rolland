88eb8fea6eec38a0d5cb14284899ed756c1c8c29
max <maximilian.mantel@tu-berlin.de>
2025-01-09 18:04:41 +0100

    upd: new Sleeper class test

--- tests/components/test_sleeper.py
+++ tests/components/test_sleeper.py
@@ -4,23 +4,10 @@ from traitlets import TraitError
 from src.components import Sleeper
 
 
-def test_sleeper_initialization():
-    """Test the initialization of the Sleeper class."""
-    sleeper = Sleeper()
-    assert sleeper.sl_typ == "B70"
-    assert sleeper.ms == 0.0
-    assert sleeper.ls == 0.0
-    assert sleeper.Bs == 0.0
-
-def test_sleeper_invalid_type():
-    """Test the initialization of the Sleeper class with invalid type."""
-    with pytest.raises(TraitError):
-        Sleeper(sl_typ=123)
-
-def test_sleeper_set_values():
-    """Test the setting of values in the Sleeper class."""
-    sleeper = Sleeper(sl_typ="B80", ms=300.0, ls=2.5, Bs=10000.0)
-    assert sleeper.sl_typ == "B80"
-    assert sleeper.ms == 300.0
-    assert sleeper.ls == 2.5
-    assert sleeper.Bs == 10000.0
+def test_sleeper_slab_initialization():
+    """Test the initialization of the Sleeper class with slab foundation."""
+    sleeper = Sleeper(fnd_type="slab")
+    assert sleeper.sl_type == "discr"
+    assert sleeper.fnd_type == "slab"
+    assert sleeper.ms == float(999999999)
+    assert sleeper.Bs == float(999999999)
