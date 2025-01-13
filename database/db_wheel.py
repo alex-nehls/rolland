400f7022e1bdf648af71a169e8c606b413511d74
max <maximilian.mantel@tu-berlin.de>
2025-01-13 15:40:14 +0100

    Revert "upd: update Wheel db"
    This reverts commit 4cacd9c865c0642212d1d6736ae9033f22ff848b.

--- database/db_wheel.py
+++ database/db_wheel.py
@@ -1,16 +1,23 @@
 """Wheel database."""
 
+from numpy import array
 
 WHEEL_DATABASE = {
     "w_type_a": {
         "mw": 400.0,
         "mw_red": 5.0,
-        "rw": 0.46
+        "rw": 0.46,
+        "dw": 0.05,
+        "w_greensf": array([[0.1, 0.2, 0.3],
+                            [0.1, 0.2, 0.3]])
     },
 
     "w_type_b": {
         "mw": 450.0,
         "mw_red": 5.5,
         "rw": 0.48,
+        "dw": 0.06,
+        "w_greensf": array([[0.1, 0.2, 0.3],
+                            [0.1, 0.2, 0.3]])
     },
 }
