7fa4d2b5d39d6b1b3b8b68f56b4f7ba3545c43a8
max <maximilian.mantel@tu-berlin.de>
2025-01-14 17:23:51 +0100

    test: add general test for components module

--- tests/components/test_ballast.py
+++
@@ -1,8 +0,0 @@
-from src.components import Ballast
-
-
-def test_ballast_initialization():
-    """Test the initialization of the Ballast class."""
-    ballast = Ballast()
-    assert ballast.sb == 0.0
-    assert ballast.db == 0.0
---
+++ tests/components/test_components.py
@@ -0,0 +1,11 @@
+import pytest
+import inspect
+import src.components as components_module
+
+def get_classes(module):
+    return [cls for name, cls in inspect.getmembers(module, inspect.isclass)]
+
+@pytest.mark.parametrize("cls", get_classes(components_module))
+def test_class_initialization(cls):
+    instance = cls()
+    assert isinstance(instance, cls)
