99186d512bc6f0bee89614370d232986f2a54aca
max <maximilian.mantel@tu-berlin.de>
2025-01-14 13:32:56 +0100

    upd: db wheel replace dic through inst

--- database/db_wheel.py
+++ database/db_wheel.py
@@ -1,23 +1,37 @@
-"""Wheel database."""
+"""Wheel type instances.
 
-from numpy import array
+This module contains instances of the Wheel class representing different wheel types.
 
-WHEEL_DATABASE = {
-    "w_type_a": {
-        "mw": 400.0,
-        "mw_red": 5.0,
-        "rw": 0.46,
-        "dw": 0.05,
-        "w_greensf": array([[0.1, 0.2, 0.3],
-                            [0.1, 0.2, 0.3]])
-    },
+Wheel types:
+    - w_type_a
+    - w_type_b
 
-    "w_type_b": {
-        "mw": 450.0,
-        "mw_red": 5.5,
-        "rw": 0.48,
-        "dw": 0.06,
-        "w_greensf": array([[0.1, 0.2, 0.3],
-                            [0.1, 0.2, 0.3]])
-    },
-}
+Attributes
+----------
+    w_prof (str): Wheel running surface profile.
+    w_geo (list): Wheel geometry coordinates [m].
+    mw (float): Wheel mass [kg].
+    mw_red (float): Reduced wheel mass (lateral dynamics) [kg].
+    rw (float): Wheel radius [m].
+"""
+
+
+from src.components import Wheel
+
+w_type_a = Wheel(
+    w_type="w_type_a",
+    w_prof="S1002",
+    w_geo=[(0.0, 0.0)],
+    mw=400.0,
+    mw_red=5.0,
+    rw=0.46
+)
+
+w_type_b = Wheel(
+    w_type="w_type_b",
+    w_prof="S1002",
+    w_geo=[(0.0, 0.0)],
+    mw=450.0,
+    mw_red=5.5,
+    rw=0.48
+)
