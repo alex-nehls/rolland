53db4e16ed0b89b455729c67e13a64b1332e63d7
esarradj <ennes.sarradj@tu-berlin.de>
2025-01-16 18:19:34 +0100

    Arranged track and example

--- example.py
+++ example.py
@@ -10,7 +10,8 @@ tr = SimplePeriodicBallastedSingleRailTrackFactory(
     count = 20
     )
 
-print(tr.factory())
+tr0 = tr.factory()
+print(tr0)
 
 tr1 = SimplePeriodicBallastedSingleRailTrack(
     rail = UIC60,
@@ -22,3 +23,19 @@ tr1 = SimplePeriodicBallastedSingleRailTrack(
     )
 
 print(tr1)
+
+pad1 = DiscrPad(sp=[1,1])
+slep1 = Sleeper(ms=200)
+pad2 = DiscrPad(sp=[2,2])
+slep2 = Sleeper(ms=300)
+
+tr2 = ArrangedBallastedSingleRailTrack(
+    rail = UIC60,
+    ballast = Ballast(),
+    pad = PeriodicArrangment(item = [pad1,pad2]),
+    sleeper = PeriodicArrangment(item = [slep1,slep2,slep2]),
+    distance = PeriodicArrangment(item = 0.49),
+    count = 20
+    )
+
+print(tr2)
