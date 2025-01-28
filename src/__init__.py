910b356a380875f1fbd6d0b6a00d0ac5c9108ee7
max <maximilian.mantel@tu-berlin.de>
2025-01-28 13:50:24 +0100

    repair

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,6 @@
 # Rolling Noise Calculation Toolbox
 # ------------------------------------------------------------------------------
 
-from .components import Pad
+from .components import Ballast, ContPad, DiscrPad, Rail, Slab, Sleeper, Wheel, WheelGreensfunc
+
+__all__ = ["Rail", "DiscrPad", "ContPad", "Sleeper", "Ballast", "Wheel", "WheelGreensfunc", "Slab"]
---
+++ tests/test_components.py
@@ -0,0 +1,14 @@
+import inspect
+
+import pytest
+
+import src.components as components_module
+
+
+def get_classes(module):
+    return [cls for name, cls in inspect.getmembers(module, inspect.isclass)]
+
+@pytest.mark.parametrize("cls", get_classes(components_module))
+def test_class_initialization(cls):
+    instance = cls()
+    assert isinstance(instance, cls)
