412dbdfe9c56a9fe69bb14d715b483c5d64075f9
max <maximilian.mantel@tu-berlin.de>
2025-02-03 14:14:52 +0100

    feat: add gaussian impuls

--- rolland/excitation.py
+++ rolland/excitation.py
@@ -1,1 +1,41 @@
 """Defines excitation."""
+from numpy import exp, linspace
+from traitlets import Float, HasTraits, Instance
+
+from rolland.grid import GridFDMStampka
+
+
+class Excitation(HasTraits):
+    """Base class for excitation."""
+
+
+class StationaryExcitation(Excitation):
+    """Stationary excitation class."""
+
+class GaussianImpulse(StationaryExcitation):
+    """Gaussian impulse excitation class."""
+
+    # Grid instance
+    grid = Instance(GridFDMStampka)
+
+    # Pulse parameter (regulates pulse-time) [-]
+    sigma = Float(default_value=0.7e-4)
+
+    # Pulse parameter (regulates amplitude) [s]
+    a = Float(default_value=0.5e2)
+
+
+    def __init__(self, *args, **kwargs):
+        """Compute force array."""
+        super().__init__(*args, **kwargs)
+        # Time array
+        t = linspace(0, self.grid.sim_t, self.grid.nt)
+
+        # Compute force array (contains force over time)
+        tg = t - 4 * self.sigma
+        self.force = self.a * tg / self.sigma ** 2 * exp(-tg ** 2 / self.sigma ** 2)
+
+
+class MovingExcitation(Excitation):
+    """Moving excitation class."""
+
