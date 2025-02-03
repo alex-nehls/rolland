ed5955f760a9de31884e96c991601742730f3b44
max <maximilian.mantel@tu-berlin.de>
2025-02-03 11:53:46 +0100

    feat: add grid method acc. to Stampka

---
+++ rolland/grid.py
@@ -0,0 +1,59 @@
+"""Grid classes."""
+from traitlets import Float, HasTraits, Instance
+
+from rolland import Track
+
+
+class GridFDMStampka(HasTraits):
+    """Calculate grid parameters for FDM simulation."""
+
+    # Track instance
+    track = Instance(Track)
+
+    # Step size in time [s]
+    dt = Float()
+
+    # Requested beam length [m]
+    req_l = Float()
+
+    # Requested simulation time [s]
+    req_simt = Float()
+
+    # Coefficient for dx calculation (must be >= 1) [-]
+    bx = Float()
+
+    # Number of spatial steps in single sided boundary domain [-]
+    n_bound = Float()
+
+
+    def __init__(self, *args, **kwargs):
+        """Compute grid parameters."""
+        super().__init__(*args, **kwargs)
+        # Number of time steps [-]
+        nt = int(self.req_simt / self.dt)
+
+        # Actual simulation time [s]
+        self.sim_t = nt * self.dt
+
+        # Minimum step size in space [m]
+        dx_min = self.bx * ((self.track.rail.E * self.track.rail.Iyr) /
+                            (6 * self.track.rail.mr)) ** (1 / 4) * self.dt ** (1 / 2)
+
+        # Updated spatial step size. Ensures that default sleeper spacing 0.6 m is a multiple of dx
+        dx = 0.6 / (0.6 // dx_min)
+
+        # Updated stability coefficient
+        self.bx_upd = dx / (((self.track.rail.E * self.track.rail.Iyr) / (6 * self.track.rail.mr)) ** (1 / 4)
+                       * self.dt ** (1 / 2))
+
+        if self.bx_upd < 1:
+            print('Calculation not valid!!! ---> Coefficient for step size must be >= 1')
+
+        # Number of spatial steps [-]
+        self.nx = int(self.req_l / dx + (2 * self.n_bound)) + 1
+
+        # Actual beam length [m]
+        self.l_domain = (self.nx - 1) * dx
+
+        # Length of boundary area [m]
+        self.l_bound = self.n_bound * dx
