3858de1427bf813d064b68e583869cf675ffd29e
wlc-alex <alexandernehls@web.de>
2025-10-24 20:32:54 +0200

    ramp up to force random force

--- rolland/excitation.py
+++ rolland/excitation.py
@@ -10,8 +10,8 @@
 """
 
 import abc
+import numpy as np
 
-from numpy import exp
 from traitlets import Float, List, Union
 
 from .abstract_traits import ABCHasTraits
@@ -58,7 +58,7 @@ class GaussianImpulse(StationaryExcitation):
     def force(self, t):
         """Compute force array (contains force over time)."""
         tg          = t - 4 * self.sigma
-        force_array = self.a*tg / self.sigma**2 * exp(-tg**2 / self.sigma**2)
+        force_array = self.a*tg / self.sigma**2 * np.exp(-tg**2 / self.sigma**2)
         return force_array
 
 
@@ -67,24 +67,43 @@ class MovingExcitation(Excitation):
 
 
 class ConstantForce(MovingExcitation):
-    """Constant force excitation class.
-
-    This excitation type is used for moving sources.
+    """Constant force excitation class for single or multiple moving loads.
 
     Attributes
     ----------
-    x_excit : float
-        Excitation position :math:`[m]`.
+    x_excit : list or float
+        Starting position(s) of excitation point(s) :math:`[m]`.
+    force_amplitude : float
+        Force amplitude per wheel :math:`[N]`.
     """
 
     force_amplitude = Float(default_value=50000.0)
-    # x_excit_start = Union([List(), Float(default_value = 50.0)])    # TODO: use this as starting point and move every timestep
-    x_excit = Union([List(), Float(default_value = 50.0)])            # NOTE: doesn't work for multiple excitation points
+    x_excit = Union([List(), Float(default_value = 50.0)])
+    velocity = Float(default_value=27.78)  # default 100 km/h in m/s
 
     def validate_excitation(self):
         """Validate excitation parameters."""
 
     def force(self, t):
         """Compute force array (contains force over time)."""
-        force_array = [self.force_amplitude] * len(t)
+        n = len(t)
+        ramp_length = int(0.05 * n)  # 10% of total length
+        force_array = []
+        
+        # Linear ramp up for first 10%
+        for i in range(ramp_length):
+            force_array.append(self.force_amplitude * (i / ramp_length))
+            
+        # Constant force with random component for remaining 90%
+        np.random.seed(42)  # für Reproduzierbarkeit
+        random_component = np.random.uniform(
+            low     = -0.1 * self.force_amplitude,
+            high    = 0.1 * self.force_amplitude,
+            size    = n - ramp_length
+        )
+        constant_part = [self.force_amplitude] * (n - ramp_length)
+
+        # force_array.extend(np.array(constant_part) + random_component)
+        force_array.extend(np.array(constant_part))
+
         return force_array
